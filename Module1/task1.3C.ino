cf9c6b2d535ddca961f22e56ced870512cfdffb4
tyghtmo <tjwrig@deakin.edu.au>
2019-03-15 12:17:10 +1100

    Add files via upload

--- Module1/task1.3C.ino
+++ Module1/task1.3C.ino
@@ -8,41 +8,51 @@ const int bledPin = 11;
 const int rledPin = 12;
 
 volatile int tiltState = 0;
-volatile int buttonState = 0;
+int buttonState = 0;
 
 void setup()
 {
-  pinMode(2, INPUT);
-  pinMode(13, OUTPUT);
-  digitalWrite(tiltPin, HIGH);
+  pinMode(tiltPin, INPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(A0, INPUT);
   
-  attachInterrupt(digitalPinToInterrupt(2), pin_tilt, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(3), pin_button, CHANGE);
-  Serial.begin(10);
+  pinMode(gledPin, OUTPUT);
+  pinMode(bledPin, OUTPUT);
+  pinMode(rledPin, OUTPUT);
+  
+  digitalWrite(tiltPin, HIGH);
+  digitalWrite(buttonPin, HIGH);
+  
+  attachInterrupt(digitalPinToInterrupt(2), pin_ISR, CHANGE);
+  Serial.begin(9600);
 }
 
 void loop()
 {
+  //Temperature loop
   int tempSensorValue = analogRead(A0);
   //Serial.println(tempSensorValue);
-
-  digitalWrite(bledPin, LOW);
-
   if(tempSensorValue > 176){
     digitalWrite(bledPin, HIGH);
+  } else {
+    digitalWrite(bledPin, LOW);
+  }
+  
+  //Button loop
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  
+  if(buttonState == HIGH){
+    digitalWrite(rledPin, HIGH);
+  } else {
+    digitalWrite(rledPin, LOW);
   }
   
   
 }
 
-void pin_tilt() {
+void pin_ISR() {
   tiltState = digitalRead(tiltPin);
   digitalWrite(gledPin, !tiltState);
   //Serial.println(tiltState);
-}
-
-void pin_button(){
-  buttonState = digitalRead(buttonPin);
-  digitalWrite(rledPin, !buttonState);
-  Serial.println(buttonState);
 }
