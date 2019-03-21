168e67585d701e000eba71ba4dd4e974dfa410de
tyghtmo <tjwrig@deakin.edu.au>
2019-03-21 11:11:11 +1100

    Changed to only update from interrupts

--- Module1/task1.3C.ino
+++ Module1/task1.3C.ino
@@ -1,3 +1,4 @@
+#include <avr/interrupt.h>
 //inputs
 const int tiltPin = 2;
 const int buttonPin = 3;
@@ -6,55 +7,46 @@ const int buttonPin = 3;
 const int gledPin = 10;
 const int bledPin = 11;
 const int rledPin = 12;
+const int led = 13;
 
 volatile int tiltState = 0;
-int buttonState = 0;
+volatile int buttonState = 0;
 
 void setup()
 {
   pinMode(tiltPin, INPUT);
   pinMode(buttonPin, INPUT);
-  pinMode(A0, INPUT);
   
   pinMode(gledPin, OUTPUT);
   pinMode(bledPin, OUTPUT);
   pinMode(rledPin, OUTPUT);
+  pinMode(led, OUTPUT);
   
   digitalWrite(tiltPin, HIGH);
   digitalWrite(buttonPin, HIGH);
   
-  attachInterrupt(digitalPinToInterrupt(2), pin_ISR, CHANGE);
-  Serial.begin(100);
+  attachInterrupt(digitalPinToInterrupt(tiltPin), tilt, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), button, CHANGE);
+  Serial.begin(9600);
+  
 }
 
 void loop()
 {
-  //Temperature loop
-  int tempSensorValue = analogRead(A0);
-  Serial.println(tempSensorValue);
-    
-  if(tempSensorValue > 176){
-    digitalWrite(bledPin, HIGH);
-  } else {
-    digitalWrite(bledPin, LOW);
-  }
-  
-    
-  //Button loop
-  buttonState = digitalRead(buttonPin);
-  Serial.println(buttonState);
-  
-  if(buttonState == HIGH){
-    digitalWrite(rledPin, HIGH);
-  } else {
-    digitalWrite(rledPin, LOW);
-  }
-  
   
 }
 
-void pin_ISR() {
+
+void tilt() {
   tiltState = digitalRead(tiltPin);
   digitalWrite(gledPin, !tiltState);
+  Serial.print("Tilt state: ");
   Serial.println(tiltState);
+}
+
+void button() {
+  buttonState = digitalRead(buttonPin);
+  digitalWrite(rledPin, buttonState);
+  Serial.print("Button state: ");
+  Serial.println(buttonState);
 }
